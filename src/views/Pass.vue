5ed8bf9d75a96329bdf76a7b74b610f47a91f7d5
hnaohiro <hasegawanaohiro1989@gmail.com>
2018-07-21 22:20:15 +0900

    add pass page

---
+++ src/views/Pass.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="pass"><img src="../assets/pass/1.png"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pass'
+}
+</script>
+
+<style>
+.pass img {
+  margin: 20px auto;
+  width: 100%;
+}
+</style>
