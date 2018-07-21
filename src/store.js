6bdcb73c4b5bdd9472e57ad4700b347ccb0ca451
hnaohiro <hasegawanaohiro1989@gmail.com>
2018-07-21 17:36:53 +0900

    add balance

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import router from './router'
-import { Connect, SimpleSigner } from 'uport-connect'
+import { Connect, SimpleSigner, MNID } from 'uport-connect'
+import Web3 from 'web3'
 
 Vue.use(Vuex)
 
@@ -11,9 +12,13 @@ const uport = new Connect('night pass', {
   signer: SimpleSigner('387342a6a3e3389aad47faf70a14f9680fb24aa38fc9565b26c3af746007b42f')
 })
 
+const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/c68e51f96f6242a9814e63ff752344d8');
+const web3 = new Web3(provider);
+
 export default new Vuex.Store({
   state: {
-    credential: null
+    credential: null,
+    balance: null
   },
   mutations: {
     setRequestToken(state, value) {
@@ -21,29 +26,52 @@ export default new Vuex.Store({
     },
     setCredential(state, value) {
       state.credential = value
+    },
+    setBalance(state, value) {
+      state.balance = value
     }
   },
   actions: {
-    requestCredential({ commit }) {
+    async requestCredential({ commit }) {
       uport.requestCredentials({
-        requested: ['name', 'phone', 'country'],
+        requested: ['name', 'avatar', 'phone', 'country'],
         notifications: true 
       })
       .then((credentials) => {
         commit('setCredential', credentials)
+        
+        const address = MNID.decode(credentials.address).address
+        web3.eth.getBalance(address)
+          .then((balance) => {
+            commit('setBalance', balance)    
+          })
+      
         router.push('user')
       })
-    }
+    },
+
   },
   getters: {
-    getName: (state) => {
+    getCredential(state) {
+      return state.credential
+    },
+    getName(state) {
       return (state.credential) ? state.credential.name : null
     },
-    getPhone: (state) => {
+    getAvatar(state) {
+      return (state.credential) ? state.credential.avatar.uri : null
+    },
+    getPhone(state) {
       return (state.credential) ? state.credential.phone : null
     },
-    getCountry: (state) => {
+    getCountry(state) {
       return (state.credential) ? state.credential.country : null
     },
+    getAddress(state) {
+      return (state.credential) ? MNID.decode(state.credential.address).address : null
+    },
+    getBalance(state) {
+      return state.balance
+    }
   }
 })
--- src/views/User.vue
+++ src/views/User.vue
@@ -2,22 +2,32 @@
   <div>
     <h1>Welcome!</h1>
     <p>name: {{getName}}</p>
+    <p>avater: <img :src="getAvatar" width="50px" height="50px"/></p>
     <p>phone: {{getPhone}}</p>
     <p>country: {{getCountry}}</p>
+    <p>balance: {{getBalance}}</p>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import router from '../router'
 
 export default {
   name: 'user',
-   computed: {
+  computed: {
     ...mapGetters([
       'getName',
+      'getAvatar',
       'getPhone',
-      'getCountry'
+      'getCountry',
+      'getBalance'
     ])
+  },
+  mounted() {
+    if (!this.$store.getters.getCredential) {
+      router.push('login')
+    }
   }
 }
 </script>
